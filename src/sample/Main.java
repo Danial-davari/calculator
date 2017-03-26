dc2b660e6c3195d5c760e77b43b6cd69d9567c7a
Luc Eleazar <lae5061@hotmail.com>
2017-03-25 22:26:35 -0400

    added event to button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -9,6 +12,12 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static sample.MathUtils.evaluate;
+
 
 public class Main extends Application {
 
@@ -16,9 +25,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception
     {
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Calculator");
         GridPane gridPane = new GridPane();
-        gridPane.setGridLinesVisible(true);
+//        gridPane.setGridLinesVisible(true);
+        gridPane.setHgap(5); //horizontal gap in pixels => that's what you are asking for
+        gridPane.setVgap(5); //vertical gap in pixels
+        gridPane.setAlignment(Pos.CENTER);
         TextField textField = new TextField();
         Button button0 = new Button("0");
         Button button1 = new Button("1");
@@ -30,12 +42,12 @@ public class Main extends Application {
         Button button7 = new Button("7");
         Button button8 = new Button("8");
         Button button9 = new Button("9");
-        Button buttondot = new Button(".");
+        Button buttonDot = new Button(".");
         Button buttonMinus = new Button("-");
         Button buttonPlus = new Button("+");
         Button buttonDivision = new Button("/");
         Button buttonMultiplication = new Button("*");
-        Button buttonResult = new Button("=");
+        Button buttonEqual = new Button("=");
         Button buttonFraction = new Button("1/x");
         Button buttonPercent = new Button("%");
         Button buttonRadical = new Button("sqrt");
@@ -52,6 +64,7 @@ public class Main extends Application {
         Font textFont = Font.font("Verdana", FontWeight.BOLD, 18);
         textField.setPrefHeight(48);
         textField.setFont(textFont);
+        textField.setAlignment(Pos.CENTER_RIGHT);
 
         GridPane.setConstraints(textField,0,0,5,1);
 
@@ -75,16 +88,15 @@ public class Main extends Application {
 
         GridPane.setConstraints(button0,0,4,2,1);
         GridPane.setConstraints(button9,2,4);
-        GridPane.setConstraints(buttondot,3,4);
+        GridPane.setConstraints(buttonDot,3,4);
         GridPane.setConstraints(buttonPlus,4,4);
 
         GridPane.setConstraints(buttonOpenParentheses,0,5);
         GridPane.setConstraints(buttonCloseParentheses,1,5);
         GridPane.setConstraints(buttonRadical,2,5);
         GridPane.setConstraints(buttonFraction,3,5);
-        GridPane.setConstraints(buttonResult,4,5);
+        GridPane.setConstraints(buttonEqual,4,5,1,2);
 
-        GridPane.setConstraints(buttonResult,4,6);
         GridPane.setConstraints(buttonSin,0,6);
         GridPane.setConstraints(buttonCos,1,6);
         GridPane.setConstraints(buttonTan,2,6);
@@ -92,22 +104,65 @@ public class Main extends Application {
 
 
 //        GridPane.setFillWidth(button0, true);
-
-
-        gridPane.getChildren().addAll(textField,buttonBackspace,buttonClear,
+        Button[] buttons = new Button[]{buttonBackspace,buttonClear,
                 buttonPercent,buttonChangePlusOrMinus,buttonMultiplication,button1,
-                button2,button3,button4,button5,button6,button7,button8,button9,button0,buttonDivision,buttonMinus
-        ,buttondot,buttonPlus,buttonOpenParentheses,buttonCloseParentheses,buttonRadical,buttonFraction,buttonResult,
-                buttonSin,buttonCos,buttonTan,buttonPower);
+                button2,button3,button4,button5,button6,button7,
+                button8,button9,button0,buttonDivision,buttonMinus
+                ,buttonDot,buttonPlus,buttonOpenParentheses,
+                buttonCloseParentheses,buttonRadical,buttonFraction,buttonEqual,
+                buttonSin,buttonCos,buttonTan,buttonPower};
+        List<Button> calculateButtons = Arrays.asList(
+                buttonPercent,buttonRadical,buttonFraction,buttonSin,buttonCos,buttonTan,buttonEqual
+        );
+        for (Button button : buttons)
+        {
+            button.setMinWidth(button==button0 ? 100 : 50);
+            button.setPadding(new Insets(5, 5, 5, 5));
+        }
+        buttonBackspace.setOnAction((ActionEvent e) ->{
+            String text = textField.getText();
+            if((text!=null) && (!text.isEmpty())){
+                textField.setText(text.substring(0,text.length()-1));
+            }
+        });
+        buttonClear.setOnAction((ActionEvent actionEvent)->{
+            textField.setText("");
+        });
+        buttonEqual.setOnAction((ActionEvent actionEvent)->{
+            String text = textField.getText();
+            if((text!=null) && (!text.isEmpty())){
+                double result = evaluate(text.trim());
+                String textResult = String.valueOf(result);
+                if(textResult.contains(".")){
+                    String[] tokens = textResult.split("\\.");
+                    long decimalPart = Long.parseLong(tokens[1]);
+                    if(decimalPart < 1){
+                        textResult = tokens[0];
+                    }
+                }
+                textField.setText(textResult);
+            }
+        });
+        for (Button button : buttons)
+        {
+            if(button==buttonBackspace || button==buttonClear || calculateButtons.contains(button)){
+                continue;
+            }
+            button.setOnAction((ActionEvent e) ->
+            {
+                String text = textField.getText();
+                textField.setText(text+button.getText());
+            });
+        }
+        buttonEqual.setMinHeight(60);
+        gridPane.getChildren().addAll(textField);
+        gridPane.getChildren().addAll(buttons);
 
         Scene scene = new Scene(gridPane, 268, 275);
-
-
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
